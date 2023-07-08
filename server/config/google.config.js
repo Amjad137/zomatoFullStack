227dde589e7db79be5a363ff135e0e0295a7dbb8
Amjad137 <mohammedamjath137@gmail.com>
2023-07-08 18:25:09 +0530

    Minor Changes

--- server/config/google.config.js
+++ server/config/google.config.js
@@ -1,43 +1,47 @@
-// import googleOAuth from "passport-google-oauth20"; //npm i passport passport-google-oauth20
-// import { userModel } from "../database/allModel";
+import googleOAuth from "passport-google-oauth20"; //npm i passport passport-google-oauth20
+import { userModel } from "../database/allModel";
+require("dotenv").config();
 
-// const GoogleStrategy = googleOAuth.Strategy;
+const GoogleStrategy = googleOAuth.Strategy;
 
-// export default (passport) => {
-//   passport.use(
-//     new GoogleStrategy(
-//       {
-//         clientID: process.env.GOOGLE_CLIENT_ID,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         callbackURL: "http://localhost:5500/auth/google/callback",
-//       },
-//       async (accessToken, refreshToken, profile, done) => {
-//         const newUser = {
-//           fullName: profile.displayName,
-//           email: profile.emails[0].value,
-//           profilePic: profile.photos[0].value,
-//         };
-//         try {
-//           const user = await userModel.findOne({ email: newUser.email });
+export default (passport) => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        // clientID:
+        //   "69735686026-eoanjp7mtbql15oa77qmbf4p7elaflsv.apps.googleusercontent.com",
+        // clientSecret: "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",
+        // callbackURL: "http://localhost:5500/auth/google/callback",
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:5500/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          fullName: profile.displayName,
+          email: profile.emails[0].value,
+          profilePic: profile.photos[0].value,
+        };
+        try {
+          const user = await userModel.findOne({ email: newUser.email });
 
-//           if (user) {
-//             const token = user.generateJwtToken();
-//             //return user
-//             done(null, { user, token });
-//           } else {
-//             //create new user
-//             const user = await userModel.create(newUser);
-//             const token = user.generateJwtToken();
-//           }
-
-//           //return user
-//           done(null, { user, token });
-//         } catch (error) {
-//           done(error, null);
-//         }
-//       }
-//     )
-//   );
-//   passport.serializeUser((userData, done) => done(null, { ...userData }));
-//   passport.deserializeUser((id, done) => done(null, id));
-// };
+          if (user) {
+            const token = user.generateJwtToken();
+            //return user
+            done(null, { user, token });
+          } else {
+            //create new user
+            const user = await userModel.create(newUser);
+            const token = user.generateJwtToken();
+            //return user
+            done(null, { user, token });
+          }
+        } catch (error) {
+          done(error, null);
+        }
+      }
+    )
+  );
+  passport.serializeUser((userData, done) => done(null, { ...userData }));
+  passport.deserializeUser((id, done) => done(null, id));
+};
